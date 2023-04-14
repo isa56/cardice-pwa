85af37cc8e78c94d7111faa884025096c0bc7c34
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2023-04-14 20:00:06 -0300

    Feat: Adiciona fontes locais

--- sw.js
+++ sw.js
@@ -1,37 +1,53 @@
 const cacheName = "cache1.0.1"; // Change value to force update
 const assets = [
   "/",
-  "android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
-  "android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
-  "android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
-  "android-chrome-96x96.png", // Favicon, Android Chrome M39+ with 2.0 screen density
-  "android-chrome-144x144.png", // Favicon, Android Chrome M39+ with 3.0 screen density
-  "android-chrome-192x192.png", // Favicon, Android Chrome M39+ with 4.0 screen density
-  "android-chrome-256x256.png", // Favicon, Android Chrome M47+ Splash screen with 1.5 screen density
-  "android-chrome-384x384.png", // Favicon, Android Chrome M47+ Splash screen with 3.0 screen density
-  "android-chrome-512x512.png", // Favicon, Android Chrome M47+ Splash screen with 4.0 screen density
-  "apple-touch-icon.png", // Favicon, Apple default
-  "apple-touch-icon-57x57.png", // Apple iPhone, Non-retina with iOS6 or prior
-  "apple-touch-icon-60x60.png", // Apple iPhone, Non-retina with iOS7
-  "apple-touch-icon-72x72.png", // Apple iPad, Non-retina with iOS6 or prior
-  "apple-touch-icon-76x76.png", // Apple iPad, Non-retina with iOS7
-  "apple-touch-icon-114x114.png", // Apple iPhone, Retina with iOS6 or prior
-  "apple-touch-icon-120x120.png", // Apple iPhone, Retina with iOS7
-  "apple-touch-icon-144x144.png", // Apple iPad, Retina with iOS6 or prior
-  "apple-touch-icon-152x152.png", // Apple iPad, Retina with iOS7
-  "apple-touch-icon-180x180.png", // Apple iPhone 6 Plus with iOS8
-  "favicon.ico", // Favicon, IE and fallback for other browsers
-  "favicon-16x16.png", // Favicon, default
-  "favicon-32x32.png", // Favicon, Safari on Mac OS
-  "logo.png", // Logo
-  "maskable_icon.png", // Favicon, maskable https://web.dev/maskable-icon
-  "mstile-70x70.png", // Favicon, Windows 8 / IE11
-  "mstile-144x144.png", // Favicon, Windows 8 / IE10
-  "mstile-150x150.png", // Favicon, Windows 8 / IE11
-  "mstile-310x150.png", // Favicon, Windows 8 / IE11
-  "mstile-310x310.png", // Favicon, Windows 8 / IE11
-  "safari-pinned-tab.svg", // Favicon, Safari pinned tab
-  "share.jpg", // Social media sharing
+  "/assets/icons/android-chrome-36x36.png", // Favicon, Android Chrome M39+ with 0.75 screen density
+  "/assets/icons/android-chrome-48x48.png", // Favicon, Android Chrome M39+ with 1.0 screen density
+  "/assets/icons/android-chrome-72x72.png", // Favicon, Android Chrome M39+ with 1.5 screen density
+  "/assets/icons/android-chrome-96x96.png", // Favicon, Android Chrome M39+ with 2.0 screen density
+  "/assets/icons/android-chrome-144x144.png", // Favicon, Android Chrome M39+ with 3.0 screen density
+  "/assets/icons/android-chrome-192x192.png", // Favicon, Android Chrome M39+ with 4.0 screen density
+  "/assets/icons/android-chrome-256x256.png", // Favicon, Android Chrome M47+ Splash screen with 1.5 screen density
+  "/assets/icons/android-chrome-384x384.png", // Favicon, Android Chrome M47+ Splash screen with 3.0 screen density
+  "/assets/icons/android-chrome-512x512.png", // Favicon, Android Chrome M47+ Splash screen with 4.0 screen density
+  "/assets/icons/apple-touch-icon.png", // Favicon, Apple default
+  "/assets/icons/apple-touch-icon-57x57.png", // Apple iPhone, Non-retina with iOS6 or prior
+  "/assets/icons/apple-touch-icon-60x60.png", // Apple iPhone, Non-retina with iOS7
+  "/assets/icons/apple-touch-icon-72x72.png", // Apple iPad, Non-retina with iOS6 or prior
+  "/assets/icons/apple-touch-icon-76x76.png", // Apple iPad, Non-retina with iOS7
+  "/assets/icons/apple-touch-icon-114x114.png", // Apple iPhone, Retina with iOS6 or prior
+  "/assets/icons/apple-touch-icon-120x120.png", // Apple iPhone, Retina with iOS7
+  "/assets/icons/apple-touch-icon-144x144.png", // Apple iPad, Retina with iOS6 or prior
+  "/assets/icons/apple-touch-icon-152x152.png", // Apple iPad, Retina with iOS7
+  "/assets/icons/apple-touch-icon-180x180.png", // Apple iPhone 6 Plus with iOS8
+  "/assets/icons/favicon.ico", // Favicon, IE and fallback for other browsers
+  "/assets/icons/favicon-16x16.png", // Favicon, default
+  "/assets/icons/favicon-32x32.png", // Favicon, Safari on Mac OS
+  "/assets/icons/maskable_icon.png", // Favicon, maskable https://web.dev/maskable-icon
+  "/assets/icons/mstile-70x70.png", // Favicon, Windows 8 / IE11
+  "/assets/icons/mstile-144x144.png", // Favicon, Windows 8 / IE10
+  "/assets/icons/mstile-150x150.png", // Favicon, Windows 8 / IE11
+  "/assets/icons/mstile-310x150.png", // Favicon, Windows 8 / IE11
+  "/assets/icons/mstile-310x310.png", // Favicon, Windows 8 / IE11
+  "/assets/icons/safari-pinned-tab.svg", // Favicon, Safari pinned tab
+  "/assets/fonts/Poppins-BoldItalic.woff2", // Fonts, Poppins Bold Italic
+  "/assets/fonts/Poppins-BoldItalic.woff", // Fonts, Poppins Bold Italic
+  "/assets/fonts/Poppins-Bold.woff2", // Fonts, Poppins Bold
+  "/assets/fonts/Poppins-Bold.woff", // Fonts, Poppins Bold
+  "/assets/fonts/Poppins-Black.woff2", // Fonts, Poppins Black
+  "/assets/fonts/Poppins-Black.woff", // Fonts, Poppins Black
+  "/assets/fonts/Poppins-Light.woff2", // Fonts, Poppins Light
+  "/assets/fonts/Poppins-Light.woff", // Fonts, Poppins Light
+  "/assets/fonts/Poppins-Medium.woff2", // Fonts, Poppins Medium
+  "/assets/fonts/Poppins-Medium.woff", // Fonts, Poppins Medium
+  "/assets/fonts/Poppins-Italic.woff2", // Fonts, Poppins Italic
+  "/assets/fonts/Poppins-Italic.woff", // Fonts, Poppins Italic
+  "/assets/fonts/Poppins-MediumItalic.woff2", // Fonts, Poppins Medium Italic
+  "/assets/fonts/Poppins-MediumItalic.woff", // Fonts, Poppins Medium Italic
+  "/assets/fonts/Poppins-Regular.woff2", // Fonts, Poppins Regular
+  "/assets/fonts/Poppins-Regular.woff", // Fonts, Poppins Regular
+  "/assets/images/logo.png", // Logo
+  "/assets/images/share.jpg", // Social media sharing
   "browserconfig.xml", // IE11 icon configuration file
   "manifest.json", // Manifest file
   "index.html", // Main HTML file
@@ -39,6 +55,8 @@ const assets = [
   "/main.js", // Main Javascript file
   "style.css", // Main CSS file
   "/style.css", // Main CSS file in folder
+  "fonts.css", // Fonts CSS file
+  "/fonts.css", // Fonts CSS file in folder
 ];
 
 self.addEventListener("install", (event) => {
